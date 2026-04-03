6253ef1e51476e726c7ff826793ed3b59d879061
Prianto <priantox07@gmail.com>
2026-04-03 17:34:00 +0600

    done

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pickle
+import numpy as np
+import pandas as pd
 
 # import the model
 pipe = pickle.load(open('../Models/pipe.pkl', 'rb'))
@@ -17,3 +19,68 @@ type = st.selectbox('Type', df['TypeName'].unique())
 ram = st.selectbox('RAM(in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
 
 # Weight
+weight = st.number_input('Weight(in kg)')
+
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
+
+# IPS
+ips = st.selectbox('IPS', ['No', 'Yes'])
+
+# Screen Size
+screen_size = st.number_input('Screen Size')
+
+# Resolution
+resolution = st.selectbox('Screen Resolution', ['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'])
+
+# CPU
+cpu = st.selectbox('CPU', df['Cpu brand'].unique())
+
+# HDD
+hdd = st.selectbox('HDD(in GB)', [0, 128, 256, 512, 1024, 2048])
+
+# SSD
+ssd = st.selectbox('SSD(in GB)', [0, 8, 128, 256, 512, 1024])
+
+# GPU
+gpu = st.selectbox('GPU', df['Gpu brand'].unique())
+
+# OS
+os = st.selectbox('OS', df['OS'].unique())
+
+if st.button('Predict Price'):
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
+    
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
+    
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
+    
+    query = pd.DataFrame({
+        'Company': [company],
+        'TypeName': [type],
+        'Ram': [ram],
+        'Weight': [weight],
+        'Touchscreen': [touchscreen],
+        'IPS': [ips],
+        'PPI': [ppi],
+        'Cpu brand': [cpu],
+        'HDD': [hdd],
+        'SSD': [ssd],
+        'Gpu brand': [gpu],
+        'OS': [os]
+    })
+
+    # BDT (1 INR = 1.32 BDT)
+    predicted_price_inr = np.exp(pipe.predict(query)[0])
+    predicted_price_bdt = predicted_price_inr * 1.32
+    
+    st.markdown(f"<h1>The predicted price is <span style='color: lightgreen;'>৳{'{:,}'.format(int(predicted_price_bdt))}</span></h1>", unsafe_allow_html=True)
